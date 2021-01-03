4bb64e403ce32da0fea0310f6fcdee0b2f352d22
kashifp <kashif.peshimam9@gmail.com>
2021-01-03 14:00:54 -0500

    Initial deploy

--- dessert-list-api/app.py
+++ dessert-list-api/app.py
@@ -2,13 +2,26 @@ from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 import os
+from strings import theString
 
 # Init app
 app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 # Database
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
+
+ENV = 'prod'
+
+
+
+if ENV == 'dev':
+    app.debug = True
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
+else:
+    app.debug = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = strings.theString
+
+
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 # Init DB
